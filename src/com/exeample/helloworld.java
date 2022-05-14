d85f5fbb97f9343a125b0c580b9e9ce2140504e9
yuutonakagawa1201 <105264573+yuutonakagawa1201@users.noreply.github.com>
2022-05-14 18:26:05 +0900

    第一課題

---
+++ src/com/exeample/helloworld.java
@@ -0,0 +1,13 @@
+package com.exeample;
+
+public class helloworld {
+    public static void main(String[] args) {
+        for (int i = 0; i < 15; i++)
+            if (i < 11) {
+                System.out.println("Hello World　" + i + "回目");
+            } else {
+                System.out.println("Hello " + i);
+            }
+
+    }
+}
